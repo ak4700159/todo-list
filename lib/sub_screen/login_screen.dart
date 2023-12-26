9578eb19a3336e8966a6195cb92397e6400610b7
seungmin <ak47001@naver.com>
2023-12-26 16:29:37 +0900

    ver 1.1

--- lib/sub_screen/login_screen.dart
+++ lib/sub_screen/login_screen.dart
@@ -1,14 +1,92 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    Widget _LoginSubFields = Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {},
+            child: const Text('회원가입'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {},
+            child: const Text('아이디 찾기'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {},
+            child: const Text('비밀 번호 찾기'),
+          ),
+        )
+      ],
+    );
+    Widget _LoginMainFields = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Expanded(
+          flex: 4,
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: '아이디',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 10),
+                  ),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: '패스워드',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: TextButton(onPressed: () {}, child: Text('로그인'),),
+        ),
+      ],
+    );
+    Widget _LoginTools = Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 4, color: Colors.black),),
+      width: MediaQuery
+          .of(context)
+          .size
+          .width * 0.9,
+      height: MediaQuery
+          .of(context)
+          .size
+          .height * 0.7,
+      child: Column(
+        children: [
+          _LoginMainFields,
+          _LoginSubFields,
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
-      ),
+        title: const Text('Login 구현')
+        ,),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [_LoginTools],
+        ),
+      )
+      ,
     );
   }
 }
--- lib/sub_screen/splash_screen.dart
+++ lib/sub_screen/splash_screen.dart
@@ -15,6 +15,7 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   void initState() {
+    print('splash create');
     controller = AnimationController(
       /// [AnimationController]s can be created with `vsync: this` because of
       /// [TickerProviderStateMixin].
@@ -30,61 +31,60 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void dispose() {
     controller.dispose();
+    print('splash delete');
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 3.0,
-                color: Colors.black,
-              ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 3.0,
+              color: Colors.black,
             ),
-            width: MediaQuery.of(context).size.width * 0.6,
-            height: MediaQuery.of(context).size.height * 0.5,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  '로딩중',
+          ),
+          width: MediaQuery.of(context).size.width * 0.6,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                '로딩중',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Colors.blueAccent,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => LoginScreen()),
+                  );
+                },
+                child: Text(
+                  '이동하기',
                   style: Theme.of(context).textTheme.titleLarge,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.blueAccent,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginScreen()),
-                    );
-                  },
-                  child: Text(
-                    '이동하기',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.transparent,
+                child: CircularProgressIndicator(
+                  strokeWidth: 8,
+                  backgroundColor: Colors.red,
+                  color: Colors.blue,
+                  value: controller.value,
+                  semanticsLabel: 'Circular progress indicator',
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.transparent,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 8,
-                    backgroundColor: Colors.red,
-                    color: Colors.blue,
-                    value: controller.value,
-                    semanticsLabel: 'Circular progress indicator',
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
